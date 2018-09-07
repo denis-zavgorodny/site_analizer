619abf860b8fe9bb510d7c7230798a164954e446
Denis Zavgorodny <deniszavgorodny@MacBook-Pro-Denis.local>
2018-09-07 19:51:48 +0300

    init

---
+++ index.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+const puppeteer = require('puppeteer');
+
+const argParams = {};
+process.argv.forEach(function (val, index) {
+    if (index > 1) {
+        const param = val.split("=");
+        if (param.length > 1) {
+            argParams[param[0]] = param[1];
+        }
+    }
+});
+
+(async (url) => {
+    if (!url) {
+        process.exit(1);
+    }
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    page.setViewport({
+        width: 1280,
+        height: 700
+    });
+    const response = await page.goto(url);
+    const responseStatus = response.status();
+    const globalMetrics = await page._client.send('Performance.getMetrics');
+
+
+    const timing = await page.evaluate(() => {
+        const result = {};
+        for (const key of Object.keys(window.performance.timing.__proto__)) {
+            result[key] = window.performance.timing[key];
+        }
+        return result;
+    });
+
+    const prepareTime = Math.abs(timing.fetchStart - timing.domainLookupStart);
+    const domainLookup = Math.abs(timing.domainLookupStart - timing.domainLookupEnd);
+    const connectionTime = Math.abs(timing.connectStart - timing.connectEnd);
+    const connectionTimeBySecure = timing.secureConnectionStart ? Math.abs(timing.secureConnectionStart - timing.connectEnd) : 0;
+    const TTFB = Math.abs(timing.requestStart - timing.responseStart);
+    const downloadTime = Math.abs(timing.responseStart - timing.responseEnd);
+    const prepareDOM = Math.abs(timing.domLoading - timing.domInteractive);
+    const evaluateInlineJS = Math.abs(timing.domInteractive - timing.domContentLoadedEventEnd);
+    const DOMContentLoaded = Math.abs(timing.domainLookupStart - timing.domContentLoadedEventEnd);
+    const pageComplete = Math.abs(timing.domainLookupStart - timing.domComplete);
+
+    const res = {
+        responseStatus,
+        prepareTime,
+        domainLookup,
+        connectionTime,
+        connectionTimeBySecure,
+        TTFB,
+        downloadTime,
+        prepareDOM,
+        evaluateInlineJS,
+        DOMContentLoaded,
+        pageComplete
+    };
+
+    console.log(JSON.stringify(res));
+
+    await browser.close();
+})(argParams.url);
