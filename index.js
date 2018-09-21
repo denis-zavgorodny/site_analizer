8dd6965a398c031772aa64eceb2d8c515825ed47
Denis Zavgorodny <deniszavgorodny@MacBook-Pro-Denis.local>
2018-09-21 15:58:49 +0300

    Добавляет эмуляция мобильной версии

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const puppeteer = require('puppeteer');
+const homedir = require('os').homedir();
 
 var log4js = require('log4js');
 log4js.configure(
@@ -11,7 +12,7 @@ log4js.configure(
                     type: 'pattern',
                     pattern: '%d [%p] [PID %z]: %m ',
                 },
-                filename: './site_metrics.log',
+                filename: `${homedir}/site_metrics.log`,
                 compress: true
             },
             out: {
@@ -30,21 +31,41 @@ var logger = log4js.getLogger('dateFile');
 logger.info(`start process`);
 
 
-const argParams = {};
+const argParams = {
+    mobile: false,
+    debug: false
+};
 process.argv.forEach(function (val, index) {
     if (index > 1) {
         const param = val.split("=");
         if (param.length > 1) {
+            switch (param[0]) {
+                case 'mobile':
+                case 'debug':
+                    param[1] = Boolean(param[1]);
+                    break;
+                default:
+                    param[1] = String(param[1]);
+                    break;
+            }
             argParams[param[0]] = param[1];
         }
     }
 });
 
-(async (url, logger) => {
+(async (argParams, logger) => {
+    const { url, mobile, debug } = argParams;
     if (!url) {
         logger.error('URL no specified');
         return;
     }
+
+    const vewport = {
+        width: 1280,
+        height: 700
+    };
+
+
     const browser = await puppeteer.launch({
         args: ['--no-sandbox'],
         timeout: 10000 // timeout to wait the browser instanse start
@@ -52,15 +73,22 @@ process.argv.forEach(function (val, index) {
     logger.info(`puppeteer was starting`);
     const page = await browser.newPage();
     logger.info(`chrome was starting`);
-    page.setViewport({
-        width: 1280,
-        height: 700
-    });
+    if (mobile === true) {
+        vewport.width = 320;
+        vewport.height = 568;
+        vewport.isMobile = true;
+        await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3');
+    }
+
+    await page.setViewport(vewport);
     var response;
     try {
         page.setDefaultNavigationTimeout(10000);
         logger.info(`chrome trying to get the page`);
         response = await page.goto(url);
+        if (debug === true) {
+            await page.screenshot({ path: 'example.png' });
+        }
         logger.info(`chrome got the page successfully`);
 
         const responseStatus = response.status();
@@ -112,4 +140,4 @@ process.argv.forEach(function (val, index) {
     }
 
 
-})(argParams.url, logger);
+})(argParams, logger);
